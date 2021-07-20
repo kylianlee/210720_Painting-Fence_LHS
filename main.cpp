233d3de0e9013f54950b18ccbb4b8132c0a466ea
Kylian <hsjgc1234@gmail.com>
2021-07-20 13:18:57 +0900

    remarking

--- main.cpp
+++ main.cpp
@@ -24,20 +24,20 @@ int main() {
 
 int paint(int s, int t){    //  s 는 시작 인덱스, t 는 마지막 인덱스 + 1
   int plankNum = t - s;
-  if(plankNum == 1)
+  if(plankNum == 1)   //  울타리가 한 개이면 페인트칠 한번
     return 1;
   int min = 1000000001;
-  for (int i = s; i < t; ++i) {
+  for (int i = s; i < t; ++i) {   //  가로의 최솟값 찾는 반복
     if(height[i] < min)
       min = height[i];
   }
-  int count = min;
+  int count = min;    //  페인트칠 최솟값만큼 함
   for (int i = s; i < t; ++i) {
     height[i] -= min;
   }
   int i = s;
   int j;
-  while(i < t){
+  while(i < t){   //  끊어진 울타리를 찾아서 재귀호출
     if(height[i] != 0){
       for (j = i + 1; j <= t; ++j) {
         if(height[j] == 0) {
